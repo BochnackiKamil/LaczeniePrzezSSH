51596122d667025c13ccf5cd70cef8ab48f98ba6
Kamil Bochnacki <bochnacki_kamil@ckziu2.edu.pl>
2026-04-10 14:32:40 +0200

    dodanie gitignore

---
+++ zadanie1/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main{
+	public static void  main(String[] args){
+		ArrayList<String> studenci = new ArrayList<>();
+		studenci.add("Ania");
+		studenci.add("Tomek");
+		studenci.add("Alex");
+
+		System.out.println("Lista studentow" + studenci);
+	}
+}
---
+++ zadanie2/Biblioteka.java
@@ -0,0 +1,7 @@
+public class Biblioteka{
+	public static void main(String[] args){
+		Ksiazka mojaKsiazka = new Ksiazka("Pan", "Tadeusz");
+
+		mojaKsiazka.wyswietl();
+	}
+}
